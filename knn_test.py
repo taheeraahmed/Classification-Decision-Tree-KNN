0ad063e04719bb3d2864840f0cfee6728bbf3d1e
Taheera Ahmed <taheera@hotmail.com>
2021-04-09 10:47:26 +0200

    Shuffle data array

--- knn_test.py
+++ knn_test.py
@@ -24,8 +24,6 @@ y_df = mushrooms_encoded_df['class']  # classes
 X_array = X_df.to_numpy()
 y_array = y_df.to_numpy()
 
-print(len(X_array))
-
 def train_test_split(X, y, test_size=0.2):
     """
     Shuffles the dataset and splits it into training and test sets.
@@ -45,17 +43,24 @@ def train_test_split(X, y, test_size=0.2):
     # This is how many rows we want our train set to be
     len_train = int((1-test_size) * len(X))+1
 
-    X_train = np.zeros(shape=())
+    #Initializing the arrays we return
+    X_train = np.zeros(shape=(len_train,22))    # 22 because of number of attributes
+    y_train = np.zeros(shape=(len_train,22))    
+    X_test = np.zeros(shape=(len_test,1))      # 1 because one class column
+    y_test = np.zeros(shape=(len_test,1)) 
 
     # Merging the class and attribute columns
     data_array = np.concatenate((X, y[:,None]), axis = 1)
-
     # Shuffling the rows in data_array
-    shuffle_data_array = np.random.shuffle(data_array)
+    np.random.shuffle(data_array)
 
     # Split the training set 
-
-
+    for row in data_array:
+        # X_train is the topmost rows (len_train) in data_array, only the first 0-22 indices       
+        print(row)
+        # y_train is the topmost rows (len_train) in data_array, only the last index
+        # X_test is the lower rows (len_test) in data_array, only the first 22 indces
+        # y_test is the lower rows (len_test) in data_array, only the first last index
     return X_train, X_test, y_train, y_test
 
 
